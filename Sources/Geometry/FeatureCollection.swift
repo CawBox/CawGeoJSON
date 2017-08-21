5ac360f89a08dcf9e4dee55c3d9295e62f54c118
Aethus Northcott <aethus@brocktonpoint.com>
2017-08-21 13:19:10 -0700

    Load feature collections + tests

--- Sources/Geometry/FeatureCollection.swift
+++ Sources/Geometry/FeatureCollection.swift
@@ -1,4 +1,27 @@
 import Foundation
 
-class FeatureCollection {
+struct FeatureCollection {
+    public let features: [Feature]
+
+    init (json: [String: Any]) throws {
+        guard let type = json [Keys.type.rawValue] as? String,
+            type == "FeatureCollection" else {
+                throw GeometryError.missingRequiredProperty (Keys.type.rawValue)
+        }
+
+        guard let features = json [Keys.features.rawValue] as? [[String: Any]] else {
+            throw GeometryError.missingRequiredProperty (Keys.features.rawValue)
+        }
+
+        self.features = try features.flatMap { feature in
+            return try Feature (json: feature)
+        }
+    }
+}
+
+extension FeatureCollection {
+    enum Keys: String {
+        case type
+        case features
+    }
 }
--- Tests/CawGeoJSONTests/CawGeoJSONTests.swift
+++ Tests/CawGeoJSONTests/CawGeoJSONTests.swift
@@ -1,16 +1,47 @@
 import XCTest
 @testable import Geometry
 
+let point: [String: Any] = [
+    "type": "Feature",
+    "geometry": [
+        "type": "Point",
+        "coordinates": [102, 0.5]
+    ]
+]
+
+let lineString: [String: Any] = [
+    "type": "Feature",
+    "geometry": [
+        "type": "LineString",
+        "coordinates": [
+            [102.0, 0.0], [103.0, 1.0], [104.0, 0.0], [105.0, 1.0]
+        ]
+    ]
+]
+
+let simplePolygon: [String: Any] = [
+    "type": "Feature",
+    "geometry": [
+        "type": "Polygon",
+        "coordinates": [
+            [[30, 10], [40, 40], [20, 40], [10, 20], [30, 10]]
+        ]
+    ]
+]
+
+let complexPolygon: [String: Any] = [
+    "type": "Feature",
+    "geometry": [
+        "type": "Polygon",
+        "coordinates": [
+            [[35, 10], [45, 45], [15, 40], [10, 20], [35, 10]],
+            [[20, 30], [35, 35], [30, 20], [20, 30]]
+        ]
+    ]
+]
+
 class CawGeoJSONTests: XCTestCase {
     func testPoint () {
-        let point: [String: Any] = [
-            "type": "Feature",
-            "geometry": [
-                "type": "Point",
-                "coordinates": [102, 0.5]
-            ]
-        ]
-
         do {
             let feature = try Feature (json: point)
             XCTAssert (feature.geometry as? Geometry.Point != nil, "Geometry was supposed to be a point")
@@ -20,16 +51,6 @@ class CawGeoJSONTests: XCTestCase {
     }
 
     func testLineString () {
-        let lineString: [String: Any] = [
-         "type": "Feature",
-            "geometry": [
-                "type": "LineString",
-                "coordinates": [
-                [102.0, 0.0], [103.0, 1.0], [104.0, 0.0], [105.0, 1.0]
-                ]
-            ]
-        ]
-
         do {
             let feature = try Feature (json: lineString)
             XCTAssert (feature.geometry as? Geometry.LineString != nil, "Geometry was supposed to be a linestring")
@@ -39,18 +60,8 @@ class CawGeoJSONTests: XCTestCase {
     }
 
     func testSimplePolygon () {
-        let polygon: [String: Any] = [
-            "type": "Feature",
-            "geometry": [
-                "type": "Polygon",
-                "coordinates": [
-                    [[30, 10], [40, 40], [20, 40], [10, 20], [30, 10]]
-                ]
-            ]
-        ]
-
         do {
-            let feature = try Feature (json: polygon)
+            let feature = try Feature (json: simplePolygon)
             guard let polygon = feature.geometry as? Geometry.Polygon else {
                 XCTFail("Geometry was supposed to be a polygon")
                 return
@@ -64,19 +75,8 @@ class CawGeoJSONTests: XCTestCase {
     }
 
     func testComplexPolygon () {
-        let polygon: [String: Any] = [
-            "type": "Feature",
-            "geometry": [
-                "type": "Polygon",
-                "coordinates": [
-                    [[35, 10], [45, 45], [15, 40], [10, 20], [35, 10]],
-                    [[20, 30], [35, 35], [30, 20], [20, 30]]
-                ]
-            ]
-        ]
-
         do {
-            let feature = try Feature (json: polygon)
+            let feature = try Feature (json: complexPolygon)
             guard let polygon = feature.geometry as? Geometry.Polygon else {
                 XCTFail("Geometry was supposed to be a polygon")
                 return
@@ -89,10 +89,33 @@ class CawGeoJSONTests: XCTestCase {
         }
     }
 
+    func testFeatureCollection () {
+        let features = [
+            point,
+            lineString,
+            simplePolygon,
+            complexPolygon
+        ]
+        let collection: [String: Any] = [
+            "type": "FeatureCollection",
+            "features": features
+        ]
+
+        do {
+            let featureCollection = try FeatureCollection (json: collection)
+            print (featureCollection.features)
+            XCTAssert (featureCollection.features.count == features.count,
+                       "Feature Collection should have \(features.count) features")
+        } catch let error {
+            XCTFail("\(error)")
+        }
+    }
+
     static var allTests = [
         ("testPoint", testPoint),
         ("testLineString", testLineString),
         ("testSimplePolygon", testSimplePolygon),
         ("testComplexPolygon", testComplexPolygon),
+        ("testFeatureCollection", testFeatureCollection),
         ]
 }
